c51850f746191592a8f21746a360da5fe3d9287a
Mike Mellenthin <mike.mellenthin@gmail.com>
2025-04-24 23:21:11 -0400

    always set IsHeaderWritten even if 0 bytes  (#4)

--- internal/response/response.go
+++ internal/response/response.go
@@ -29,8 +29,6 @@ func (rw *ResponseWriter) WriteHeader(code int) {
 func (rw *ResponseWriter) Write(body []byte) (int, error) {
 	bytesWritten, err := rw.ResponseWriter.Write(body)
 	rw.BytesWritten += int64(bytesWritten)
-	if rw.BytesWritten > 0 {
-		rw.IsHeaderWritten = true
-	}
+	rw.IsHeaderWritten = true
 	return bytesWritten, err
 }
